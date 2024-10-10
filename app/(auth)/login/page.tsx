56361c60ea7af9556d1945a4f0bf0cee8190ec67
Kwasi  Addo Nyarko <kwasiaddonyarkojunior@gmail.com>
2024-10-10 15:30:37 +0000

    With Middleware

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,18 @@
 import { LoginForm } from '@/components/custom/LoginForm';
+import { authOptions } from '@/lib/config/authOptions';
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation';
 
 const LoginPage = async () => {
+
+
+  const session = await getServerSession(authOptions);
+    
+  if (session && session.user.isAdmin) {
+    return redirect('/admin/dashboard/users'); // Or any other route you want to redirect non-admin users to
+  }else if(session && !session.user.isAdmin){
+    return redirect('/investor/dashboard/profile');
+  }
 
   return (
     <section className="flex flex-col w-full h-screen justify-center items-center">
