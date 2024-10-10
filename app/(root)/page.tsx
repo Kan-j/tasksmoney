49bf170ad0f0a3826f6cc0b7f4c4e586c9e7b73e
Kwasi  Addo Nyarko <kwasiaddonyarkojunior@gmail.com>
2024-10-10 11:54:26 +0000

    I don't care about hosting

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,16 @@
+import { authOptions } from '@/lib/config/authOptions';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const Home = () => {
+const Home = async() => {
+    const session = await getServerSession(authOptions);
+
+    if (session && session.user.isAdmin) {
+      return redirect('/admin/dashboard/users'); // Or any other route you want to redirect non-admin users to
+    }else if(session && session.user.isAdmin){
+      return redirect('/investor/dashboard/profile');
+    }
   return (
     <div>Home</div>
   )
