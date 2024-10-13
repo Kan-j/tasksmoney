2923a43c17e3e0f23c0d4da650235dd0defc2e0a
Kwasi  Addo Nyarko <kwasiaddonyarkojunior@gmail.com>
2024-10-13 22:52:00 +0000

    Recharge table mod

--- components/custom/RechargeRequestTable.tsx
+++ components/custom/RechargeRequestTable.tsx
@@ -39,7 +39,7 @@ const RechargeRequestTable = ({ rechargeRequests }: { rechargeRequests: any[] })
 
   return (
     <div className="w-full mt-8">
-      <div className="overflow-x-auto sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl rounded-lg shadow-md">
+      <div className="overflow-hidden w-[300px] sm:w-[500px] md:w-[700px] shadow-md">
         <Table className="min-w-full  border border-gray-200">
           {/* Table Head */}
           <TableHeader>
